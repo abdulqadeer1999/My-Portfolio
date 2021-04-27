271bf0df6971faed10aaf6deebafa316ae7e537f
abdulqadeer1999 <abdulqadeersolangi007@gmail.com>
2021-04-27 17:59:31 +0500

    sidebar added

---
+++ src/components/Header.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { makeStyles } from "@material-ui/core/styles"
+import avatar from "../avatar.png"
+import Typed from "react-typed"
+import  {Typography,Avatar, Grid, Box} from "@material-ui/core"
+
+// CSS Style
+
+
+const useStyles = makeStyles (theme=> ({
+      avatar: {
+          width:theme.spacing(15),
+          height:theme.spacing(15),
+          margin:theme.spacing(1),
+      },
+      title : {
+               color:"tomato"
+      },
+      subTitel : {
+          color : "tan",
+           marginBottom:"3rem"
+      },
+      typedConatiner  : {
+          position:"absolute",
+          top : "50%",
+          left : "50%",
+          transform : "translate(-50%,-50%)",
+          width : "100vw",
+          textAlign : "center",
+          zIndex : 1
+
+      }
+}))
+
+function Header() {
+
+    const classes = useStyles ()
+    return (
+        <Box className ={classes. typedConatiner}>
+            <Avatar className ={classes.avatar} src={avatar} alt="qadeer pic" />
+            <Grid container justify="center"></Grid>
+            
+            <Typography className ={classes.title} variant="h4">
+
+                <Typed strings={["Abdul Qadeer"]} typeSpeed={40} />
+
+            </Typography>
+            <br />
+            <Typography className ={classes.subTitle} variant="h5">
+
+                <Typed strings={["Web Design","Web Development","MERN Stack"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop />
+
+            </Typography>
+        </Box>
+    )
+}
+
+export default Header
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React ,{ useState } from "react"
+import MobilRightMenuSlider from "@material-ui/core/Drawer"
 
 import {
       AppBar,
@@ -30,7 +31,7 @@ const useStyles = makeStyles (theme =>({
     menuSliderContainer : {
         width:250,
         background:"#511",
-        height : "30rem"
+        height : "100%"
 },
     avatar : {
         display : "block",
@@ -38,33 +39,87 @@ const useStyles = makeStyles (theme =>({
         width: theme.spacing(13),
         height:theme.spacing(13) 
     },
+    ListItem : {
+        color: "tan",
+
+    }
 }))
+
+
+const menuItems = [
+    {
+        listIcon : <Home />,
+        listText : "Home"
+    },
+    {
+        listIcon : <AssignmentInd />,
+        listText : "Resume"
+    },
+    {
+        listIcon : <Apps />,
+        listText : "Portfolio"
+    },
+    {
+        listIcon : <ContactMail />,
+        listText : "Contacts"
+    }
+]
 
 const Navbar = () => {
 
+    const [state,setState] = useState({
+        right:false
+    }) 
+
+    const togglerSlider = ((slider,open) => () => {
+        setState({...state,[slider]:open})
+    });
+
     const classes = useStyles()
+    const sideList = slider => (
+        <Box
+        
+        className = {classes.menuSliderContainer} component="div"
+        onClick={togglerSlider(slider,false)}
+        >
+        <Avatar className = {classes.avatar} src={avatar} alt="portfolio pic" />
+        <Divider />
+        <List>
+            {menuItems.map((lsItem,key)=> (
+               <ListItem button key= {key}>
+               <ListItemIcon className={classes.ListItem}> 
+                   {lsItem.listIcon}
+               </ListItemIcon>
+                   <ListItemText className={classes.ListItem} primary={lsItem.listText} />
+                   
+            </ListItem>
+                  
+            ))}
+            
+                     
+             
+        </List>
+      </Box>
+    )
     return (
         <div>
-            <Box className = {classes.menuSliderContainer} component="div">
-              <Avatar className = {classes.avatar} src={avatar} alt="portfolio pic" />
-              <Divider />
-              <List>
-                  <ListItem>
-                      <ListItemIcon>
-                          
-                      </ListItemIcon>
-                  </ListItem>
-              </List>
-            </Box>
+          
         <Box component="nav">
           <AppBar position="static" style={{background:"#222"}}>
               <Toolbar>
-                  <IconButton>
+                  <IconButton onClick={togglerSlider("right",true)}>
                   <ArrowBack style = {{color:"tomato"}} />
                   </IconButton>
                   <Typography variant="h5" style= {{color:"tan"}}>
                     Portfolio
                   </Typography>
+                  <MobilRightMenuSlider 
+                  anchor="right"
+                  open = {state.right} 
+                  onClose ={togglerSlider("right",false)}
+                  >
+                  {sideList("right")}
+                  </MobilRightMenuSlider>
                       
               </Toolbar>
           </AppBar>
